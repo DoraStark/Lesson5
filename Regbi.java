f01b9165a62efed35d8546b3e334d9424e319707
DoraStark <156120925+DoraStark@users.noreply.github.com>
2024-01-15 21:46:13 +0100

    Update Regbi.java
    Corrected

--- Regbi.java
+++ Regbi.java
@@ -11,11 +11,13 @@ public class Regbi {
         int minAge = 18;
         int maxAge = 40;
         int average1=0;
-        int average2 =0;
+        int average2=0 ;
 
         for(int i=0; i<team1.length; i++){
             team1[i] = random.nextInt(maxAge-minAge)+minAge;
         }
+
+        System.out.println("Вік гравців першої команди: "+(Arrays.toString(team1)));
 
         for (int age1 :team1) {
             average1+=age1;}
@@ -27,16 +29,32 @@ public class Regbi {
             team2[i] = random.nextInt(maxAge-minAge)+minAge;
         }
 
+        System.out.println("Вік гравців другої команди: "+(Arrays.toString(team2)));
+
         for (int age2 :team2) {
             average2+=age2;
         }
 
         average2/=team2.length;
-        
+
+
+
 
         System.out.println("Середній вік гравців першої команди: "+(average1));
+
         System.out.println("Середній вік гравців другої команди: "+(average2));
 
         }
     }
+
+
+
+
+
+
+
+
+
+
+
 
